e2b81967492eb74fcdb74de10b17f77841d17a9f
Tara <mtaraalleah@gmail.com>
2026-09-02 15:11:33 +0300

    Converted Hero,About,Services,Tours,Footer from HTML to JSX as well as added images.

--- week3-fe-pair-programming/src/components/Footer.jsx
+++ week3-fe-pair-programming/src/components/Footer.jsx
@@ -1,11 +1,65 @@
 function Footer() {
   return (
-    <div>
-      <p className="section-dummy">Footer</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <footer className="section footer">
+      <ul className="footer-links" id="nav-links">
+        <li>
+          <a href="#home" className="footer-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="footer-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="footer-link">
+            services
+          </a>
+        </li>
+        <li>
+          <a href="#tours" className="footer-link">
+            tours
+          </a>
+        </li>
+      </ul>
+      <ul className="footer-icons">
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-facebook" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-twitter" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.squarespace.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-icon"
+          >
+            <i className="fab fa-squarespace" />
+          </a>
+        </li>
+      </ul>
+      <p className="copyright">
+        copyright © travel tours company <span id="date" />. all rights reserved
+      </p>
+    </footer>
   );
 }
 
 export default Footer;
- 
--- week3-fe-pair-programming/src/components/Services.jsx
+++ week3-fe-pair-programming/src/components/Services.jsx
@@ -1,11 +1,51 @@
 function Services() {
   return (
-    <div>
-      <p className="section-dummy">Services</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <section className="section services" id="services">
+      <div className="section-title">
+        <h2>
+          our <span>services</span>
+        </h2>
+      </div>
+      <div className="section-center services-center">
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-wallet fa-fw" />
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">saving money</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+              officia.
+            </p>
+          </div>
+        </article>
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-tree fa-fw" />
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">endless hiking</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+              officia.
+            </p>
+          </div>
+        </article>
+        <article className="service">
+          <span className="service-icon">
+            <i className="fas fa-socks fa-fw" />
+          </span>
+          <div className="service-info">
+            <h4 className="service-title">amazing comfort</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+              officia.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
 export default Services;
- 

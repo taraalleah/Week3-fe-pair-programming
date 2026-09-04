805c8535e6e3cabf6b84f2c0f72cc77aa7f1dd7d
Tara <mtaraalleah@gmail.com>
2026-09-04 12:56:42 +0300

    Iteration 8 - optional

--- week3-fe-pair-programming/src/components/Hero.jsx
+++ week3-fe-pair-programming/src/components/Hero.jsx
@@ -1,13 +1,15 @@
+import styles from './Hero.module.css'
+
 const Hero = () => {
   return (
-    <section className='hero' id='home'>
-      <div className='hero-banner'>
+    <section className={styles.hero} id='home'>
+      <div className={styles['hero-banner']}>
         <h1>backroads app</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
         </p>
-        <a href='#tours' className='btn hero-btn'>
+        <a href='#tours' className={`btn ${styles['hero-btn']}`}>
           explore tours
         </a>
       </div>
